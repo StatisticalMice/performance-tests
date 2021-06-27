1129a63efbe35c67a6b5c5940034b01a18f46c68
Ari <ari.huttunen@me.com>
2021-06-27 11:55:53 +0300

    updated file serving

--- GenieApp/routes.jl
+++ GenieApp/routes.jl
@@ -1,17 +1,21 @@
-using Genie.Router, Genie.Renderer, Genie.Exceptions, JSON3
+using Genie.Router, Genie.Renderer, Genie.Exceptions, Genie.Renderer.Json
 
 route("/") do
   serve_static_file("welcome.html")
 end
 
+const GREETING = "Welcome to Genie! This site is served by Julia $(Base.VERSION) with $(Threads.nthreads()) threads."
+
 route("/greeting", named = :greeting) do
-  "Welcome to Genie! This site is served by Julia $(Base.VERSION) with $(Threads.nthreads()) threads."
+  html(GREETING)
 end
 
 route("/random", named = :random) do
- num = 1
- if haskey(@params, :num)
-   num = parse(Int64, @params(:num))
- end
- JSON3.write(rand(Int, num))
+  p = haskey(@params, :num) ? @params(:num) : "1"
+  num = tryparse(Int32, p)
+  if (num === nothing || num < 1) 
+    num = 1
+  end
+
+  json(rand(Int, num))
 end
--- http_server.jl
+++ http_server.jl
@@ -1,20 +1,32 @@
+using HTTP: @register
 using HTTP, Sockets, JSON3, Logging
 using HTTP.URIs: queryparams, URI
 
 const ROUTER = HTTP.Router()
 
 function serveWelcome(req::HTTP.Request)
-    return HTTP.Response(200, "Welcome to Genie! This site is served by Julia $(Base.VERSION) with $(Threads.nthreads()) threads.")
+    return HTTP.Response(200, "welcome.html")
+end
+
+const GREETING = "Welcome to Genie! This site is served by Julia $(Base.VERSION) with $(Threads.nthreads()) threads."
+
+function serveGreeting(req::HTTP.Request)
+    return HTTP.Response(200, GREETING)
 end
 
 function serveRandom(req::HTTP.Request)
-    num = parse(Int, get(queryparams(URI(req.target)), "num", "1"))
+    p = get(queryparams(URI(req.target)), "num", "1")
+    num = tryparse(Int32, p)
+    if (num === nothing || num < 1) 
+        num = 1
+    end
 
     return HTTP.Response(200, JSON3.write(rand(Int, num)))
 end
 
 HTTP.@register(ROUTER, "GET", "/", serveWelcome)
+HTTP.@register(ROUTER, "GET", "/greeting", serveGreeting)
 HTTP.@register(ROUTER, "GET", "/random", serveRandom)
 
-Logging.global_logger(NullLogger())
+#Logging.global_logger(NullLogger())
 HTTP.serve(ROUTER, Sockets.localhost, 8000)
