733ef92e22cf586c6f15874c83ece875d8e76d2e
Ari <ari.huttunen@me.com>
2021-06-27 14:16:18 +0300

    Don't test reading of welcome.html

--- GooseAttack/src/main.rs
+++ GooseAttack/src/main.rs
@@ -3,7 +3,6 @@ use goose::prelude::*;
 fn main() -> Result<(), GooseError> {
     let _goose_metrics = GooseAttack::initialize()?
         .register_taskset(taskset!("LoadtestTasks")
-            .register_task(task!(loadtest_welcome_html).set_name("welcome.html").set_weight(1)?)
             .register_task(task!(loadtest_greeting).set_name("greeting").set_weight(10)?)
             .register_task(task!(loadtest_random_1000).set_name("random?num=1000").set_weight(10)?)
         )
@@ -11,12 +10,6 @@ fn main() -> Result<(), GooseError> {
 
     Ok(())
 }
-
-async fn loadtest_welcome_html(user: &GooseUser) -> GooseTaskResult {
-    let _goose = user.get("/").await?;
-
-    Ok(())
-}   
 
 async fn loadtest_greeting(user: &GooseUser) -> GooseTaskResult {
     let _goose = user.get("/greeting").await?;
