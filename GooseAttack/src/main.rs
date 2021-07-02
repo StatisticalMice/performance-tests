42c33e699330a98eaa29a1f086469c3641b45c1f
Ari <ari.huttunen@me.com>
2021-07-02 10:20:46 +0300

    Geese wait 1-2 seconds between requests

--- GooseAttack/src/main.rs
+++ GooseAttack/src/main.rs
@@ -3,6 +3,7 @@ use goose::prelude::*;
 fn main() -> Result<(), GooseError> {
     let _goose_metrics = GooseAttack::initialize()?
         .register_taskset(taskset!("LoadtestTasks")
+            .set_wait_time(1, 2).unwrap()
             .register_task(task!(loadtest_greeting).set_name("greeting").set_weight(10)?)
             .register_task(task!(loadtest_random_1000).set_name("random?num=1000").set_weight(10)?)
         )
